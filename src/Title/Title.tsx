4d4e3874ae7c7fd0886b6928263859513c218115
AlexisMoretto <alexis.moretto@oclock.school>
2024-08-20 13:30:25 +0200

    Redimensionnement des img

--- src/Title/Title.tsx
+++ src/Title/Title.tsx
@@ -22,14 +22,14 @@ function Title ({experienceId}:TitleProps) {
                     <a href="https://www.linkedin.com/in/alexis-moretto-846503118/"><img src="\linkedin-icon.svg" alt="" /></a>
                 </div>
                 <div className='github'>
-                    <a href="https://github.com/AlexisMoretto"><img src="\github-icon-1.svg" alt="" /></a>
+                    <a href="https://github.com/AlexisMoretto"><img src="github-mark-white.svg" alt="" /></a>
                 </div>
                 <div className='message'>
                     <a href=""><img src="\official-gmail-icon-2020-.svg" alt="" /></a>
                 </div>
                 </div>
                 <div className='arrowDown'>
-                    <button className='arrowDownButton' onClick={handleScrollToComponent}><img src="\down-arrow-svgrepo-com.svg" alt="" /></button>
+                    <button className='arrowDownButton' onClick={handleScrollToComponent}><img className='arrowDownImg' src="\down-arrow-svgrepo-com.svg" alt="" /></button>
                 </div>
             </div>
         </div>
